a1a4b5b7db0b252a0829ea38ce4270505882c2d3
Mahbubullah <mahbubullahpathan@gmail.com>
2023-09-03 20:59:40 +0600

    init

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: ["./src/**/*.{html,js}"],
+    theme: {
+        extend: {
+            colors: {
+
+                'primary': '#E527B2',
+
+            },
+        },
+    },
+    plugins: [],
+}
